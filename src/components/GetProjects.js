dbbd7b698f1e627ebd1ecca4c45d6e8dcf629a8e
Aakarsh029 <20bcs007@nith.ac.in>
2023-02-12 14:22:40 +0530

    desktop version created

---
+++ src/components/GetProjects.js
@@ -0,0 +1,25 @@
+import img1 from "./images/1.jpg";
+const projects = [
+    {
+        id: 1,
+        image: img1,
+        name: "Watsapp Mern",
+        category: "web",
+        description: "lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum "
+    },
+    {
+        id: 2,
+        image: img1,
+        name: " Mern",
+        category: "web",
+        description: "lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum "
+    },
+    {
+        id:2,
+        image: img1,
+        name: "Watsapp ",
+        category: "ml",
+        description: "lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum "
+    }
+]
+export default projects;
--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,8 +1,81 @@
-import React from 'react'
-import "./css/Project.css"
+import React, { useState , useEffect } from 'react'
+import "./css/Project.css";
+import projects from './GetProjects';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 function Project() {
+  const [items, setItems] = useState(projects);
+  const filterItem = (cateItem) => {
+    const updatedItems = projects.filter((curele) => {
+      return curele.category === cateItem;
+    });
+    setItems(updatedItems);
+  }
+  
+  const filter = document.querySelectorAll(".filter");
+    var sliderNav = function (manual) {
+      filter.forEach((filter) => {
+        filter.classList.remove("active");
+      })
+      filter[manual].classList.add("active");
+    }
+    filter.forEach((filter, i) => {
+      filter.addEventListener("click", () => {
+        sliderNav(i);
+      })
+    })
+
+
+
+
+  useEffect(() => {
+    AOS.init();
+  },[])
+
+
+
   return (
-    <div className='Project'>Project</div>
+    <div className="Project">
+      <div className="Project__heading">Project</div>
+      <hr />
+      <div className="Project__content">I have
+        started Web Development quite recently
+        and have developed many
+        projects apart from this portfolio website.
+        So, Here are some of my projects:</div>
+      <div className="Project__tabs">
+        <ul id="Project__tab__filters">
+          <li className="filter active" onClick={() => setItems(projects)}>All</li>
+          <li className='filter' onClick={() => filterItem('web')} >Web</li>
+          <li className='filter' onClick={() => filterItem('ml')}>ML</li>
+        </ul>
+      </div>
+      <div className="Project__container">
+
+        {
+          items.map((element) => {
+            const { name, image, description } = element;
+            return (
+              <div className="Project__holder aos-init aos-animate" data-aos="zoom-in">
+                <div className="Project__details">
+                  <div className="Project__image">
+                    <img src={image} alt="" />
+                  </div>
+                  <div className="Project__info">
+                    <div className="Project__info__heading">{name}</div>
+                    <div className="Project__info__content">
+                      {description}
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+            )
+          })
+        }
+      </div>
+      
+    </div>
   )
 }
 
