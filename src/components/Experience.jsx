8383efe18a46fbd169e85666e14b0f417a301ec1
Aakarsh029 <20bcs007@nith.ac.in>
2023-04-21 18:08:22 +0530

    certificate update

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import "./css/Experience.css";
 import fei from"./images/FrontEndInternship.png";
+import ri from "./images/AakarshResearchCertificate.jpg";
 export default function Experience() {
   return (
     <div className='Experience' id='Experience'>
@@ -8,6 +9,9 @@ export default function Experience() {
         <hr />
         <div className="Experience__Certificates">
             <a href={fei} target='_blank'><img src={fei} alt="" /></a>
+            <a target='_blank' href="https://drive.google.com/file/d/1JXP9O-kDQarMnf-J1WiXexhDy2lwgaf_/view?usp=share_link">
+              <img id='experience__certificate__2' src={ri} alt="" />
+            </a>
         </div>
     </div>
   )
