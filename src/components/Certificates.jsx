e03412cbdab748b41697ecbd4428ffae3a958d09
Aakarsh029 <aakarshbansal2002@gmail.com>
2024-05-29 11:26:35 +0530

    update!!!

--- src/components/Certificates.jsx
+++ src/components/Certificates.jsx
@@ -9,7 +9,8 @@ import cert6 from "./Club/certi_page-0006.jpg";
 import e1 from "./Club/Aakarsh first year Ecell.png";
 import iste from "./Club/iste.png"
 import app1 from "./Club/App Team Final Yr Certificate.png"
-
+import c1 from "./courses/CertificateOfCompletion_Level Up Advanced SQL_page-0001.jpg";
+import c2 from "./courses/CertificateOfCompletion_SQL Server Fundamentals Master Basic Query Techniques_page-0001.jpg"
 export default function Certificates() {
   return (
     <div className='Certificates' id='Certificates'>
@@ -38,6 +39,14 @@ export default function Certificates() {
             <a href="https://drive.google.com/file/d/1sl_emfKwCjbWN1jSKBfvECzBjASjAuIO/view?usp=sharing" target='_blank'><img src={cert4} alt="" /></a>
             <a href="https://drive.google.com/file/d/1R-GjUgRT78XpEux8lRtm_TPSPlOEf5kG/view?usp=sharing" target='_blank'><img src={iste} alt="" /></a>
         </div>
+
+
+        <div className="Project__heading">Completed Courses Cetificates</div>
+        <hr />
+        <div className="Certificates__Certificates">
+            <a href="https://drive.google.com/file/d/18jkCheb-e0awOu5-4WEU_dSAVD8Yc6Pj/view?usp=sharing" target='_blank'><img src={c1} alt="" /></a>
+            <a href="https://drive.google.com/file/d/1GPzaxqWohhldYo3WdH8fc72KUO9B6Fcc/view?usp=sharing" target='_blank'><img src={c2} alt="" /></a>
+            </div>
     </div>
   )
 }
